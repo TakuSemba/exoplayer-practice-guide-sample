e81f63eafe1d9867d5decb4e55982a8af2fc08d2
takusemba <takusemba.ele@gmail.com>
2020-04-26 15:19:39 +0900

    initial commit

---
+++ app/src/main/java/com/takusemba/exobook/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.takusemba.exobook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
