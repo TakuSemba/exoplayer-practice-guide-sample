a691a775676ac6fbb2c3def96fed5c495fcc49ab
takusemba <takusemba.ele@gmail.com>
2020-04-27 17:19:40 +0900

    chapter 6

--- app/src/main/java/com/takusemba/exobook/core/UiSampleActivity.kt
+++ app/src/main/java/com/takusemba/exobook/core/UiSampleActivity.kt
@@ -1,7 +1,55 @@
 package com.takusemba.exobook.core
 
+import android.net.Uri
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import com.takusemba.exobook.R
 
 class UiSampleActivity : AppCompatActivity() {
 
+    private val player by lazy { SimpleExoPlayer.Builder(this).build() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ui)
+
+        val playerView = findViewById<PlayerView>(R.id.player_view)
+        playerView.player = player
+
+        val userAgent = Util.getUserAgent(this, "SampleApp")
+        val dataSourceFactory = DefaultDataSourceFactory(this, userAgent)
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(URI)
+
+        player.setAudioAttributes(AudioAttributes.DEFAULT, true)
+        player.prepare(mediaSource)
+        player.playWhenReady = true
+    }
+
+    override fun onStart() {
+        super.onStart()
+        player.playWhenReady = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player.playWhenReady = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player.release()
+    }
+
+    companion object {
+
+        private val URI =
+            Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+    }
 }
