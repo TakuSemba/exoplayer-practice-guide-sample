6f86ad808233f134ad473f9336462b1ca9d7b238
takusemba <takusemba.ele@gmail.com>
2020-04-29 12:19:33 +0900

    add download smaple

--- app/src/main/java/com/takusemba/exobook/core/DownloadSampleActivity.kt
+++ app/src/main/java/com/takusemba/exobook/core/DownloadSampleActivity.kt
@@ -1,7 +1,57 @@
 package com.takusemba.exobook.core
 
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.offline.DownloadRequest
+import com.google.android.exoplayer2.offline.DownloadService
+import com.takusemba.exobook.App
+import com.takusemba.exobook.R
+import com.takusemba.exobook.extension.SchedulerSampleService
 
 class DownloadSampleActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button)
+
+        val button = findViewById<Button>(R.id.button)
+        button.text = getString(R.string.button_toggle_download)
+        button.setOnClickListener {
+            val downloadManager = (application as App).downloadManager
+            val download = downloadManager.downloadIndex.getDownload(CONTENT_ID)
+            if (download != null) {
+                DownloadService.sendRemoveDownload(
+                    this,
+                    SchedulerSampleService::class.java,
+                    CONTENT_ID,
+                    false
+                )
+            } else {
+                val downloadRequest = DownloadRequest(
+                    CONTENT_ID,
+                    DownloadRequest.TYPE_PROGRESSIVE,
+                    URI,
+                    emptyList(),
+                    null,
+                    null
+                )
+                DownloadService.sendAddDownload(
+                    this,
+                    DownloadSampleService::class.java,
+                    downloadRequest,
+                    false
+                )
+            }
+        }
+    }
+
+    companion object {
+
+        private const val CONTENT_ID = "big_buck_bunny"
+
+        private val URI =
+            Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+    }
 }
---
+++ app/src/main/java/com/takusemba/exobook/core/DownloadSampleService.kt
@@ -0,0 +1,75 @@
+package com.takusemba.exobook.core
+
+import android.app.Notification
+import com.google.android.exoplayer2.offline.Download
+import com.google.android.exoplayer2.offline.DownloadManager
+import com.google.android.exoplayer2.offline.DownloadService
+import com.google.android.exoplayer2.scheduler.PlatformScheduler
+import com.google.android.exoplayer2.scheduler.Scheduler
+import com.google.android.exoplayer2.util.NotificationUtil
+import com.takusemba.exobook.App
+import com.takusemba.exobook.R
+
+class DownloadSampleService : DownloadService(
+    NOTIFICATION_ID,
+    DEFAULT_FOREGROUND_NOTIFICATION_UPDATE_INTERVAL,
+    App.CHANNEL_ID,
+    R.string.channel_name,
+    R.string.channel_description
+) {
+
+    private val downloadListener = object : DownloadManager.Listener {
+        override fun onDownloadChanged(
+            downloadManager: DownloadManager,
+            download: Download
+        ) {
+            val notification: Notification = when (download.state) {
+                Download.STATE_COMPLETED -> {
+                    (application as App).notificationHelper.buildDownloadCompletedNotification(
+                        android.R.drawable.stat_sys_download_done, null, null
+                    )
+                }
+                Download.STATE_FAILED -> {
+                    (application as App).notificationHelper.buildDownloadFailedNotification(
+                        android.R.drawable.stat_sys_download_done, null, null
+                    )
+                }
+                else -> return
+            }
+            NotificationUtil.setNotification(
+                this@DownloadSampleService,
+                NOTIFICATION_ID + 1,
+                notification
+            )
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        downloadManager.addListener(downloadListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        downloadManager.removeListener(downloadListener)
+    }
+
+    override fun getDownloadManager(): DownloadManager {
+        return (application as App).downloadManager
+    }
+
+    override fun getForegroundNotification(downloads: MutableList<Download>): Notification {
+        return (application as App).notificationHelper
+            .buildProgressNotification(android.R.drawable.stat_sys_download, null, null, downloads)
+    }
+
+    override fun getScheduler(): Scheduler? {
+        return PlatformScheduler(this, JOB_ID)
+    }
+
+    companion object {
+
+        private const val NOTIFICATION_ID = 1
+        private const val JOB_ID = 1
+    }
+}
